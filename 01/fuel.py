dbdf34f609165f586315dd226b3c0357ae1938c5
Sebastian Höffner <info@sebastian-hoeffner.de>
2019-12-03 03:52:42 +0100

    Second fuel exercise.

--- 01/fuel.py
+++ 01/fuel.py
@@ -1,3 +1,11 @@
 import sys
 
-print(sum(int(line) // 3 - 2 for line in sys.stdin))
+module_fuels = [int(line) // 3 - 2 for line in sys.stdin]
+fuel = []
+for module_fuel in module_fuels:
+    fuel.append(module_fuel)
+    fuel_fuel = fuel[-1] // 3 - 2
+    while fuel_fuel > 0:
+        fuel[-1] += fuel_fuel
+        fuel_fuel = fuel_fuel // 3 - 2
+print(sum(module_fuels), sum(fuel))
