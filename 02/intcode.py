290035ae2b3cad67c7789b50addd352cdb93ef21
Sebastian Höffner <info@sebastian-hoeffner.de>
2019-12-03 04:00:23 +0100

    Second gravity computer exercise.

--- 02/intcode.py
+++ 02/intcode.py
@@ -1,19 +1,31 @@
 import sys
 
 memory = [int(x) for x in sys.stdin.read().split(',')]
-memory[1], memory[2] = 12, 2
 
-op_idx = 0
-while memory[op_idx] in [1, 2, 99]:
-    opcode = memory[op_idx]
-    if opcode == 99:
-        print(memory[0])
-        sys.exit(0)
-    r0, r1, r2 = op_idx + 1, op_idx + 2, op_idx + 3
-    if opcode == 1:
-        memory[memory[r2]] = memory[memory[r0]] + memory[memory[r1]]
-    elif opcode == 2:
-        memory[memory[r2]] = memory[memory[r0]] * memory[memory[r1]]
-    op_idx += 4
-else:
-    sys.exit(1)
+
+def run(memory, noun, verb, control):
+    memory[1], memory[2] = noun, verb
+    op_idx = 0
+    while memory[op_idx] in [1, 2, 99]:
+        opcode = memory[op_idx]
+        if opcode == 99:
+            if memory[0] == control:
+                return 100 * noun + verb
+            else:
+                return False
+        r0, r1, r2 = op_idx + 1, op_idx + 2, op_idx + 3
+        if opcode == 1:
+            memory[memory[r2]] = memory[memory[r0]] + memory[memory[r1]]
+        elif opcode == 2:
+            memory[memory[r2]] = memory[memory[r0]] * memory[memory[r1]]
+        op_idx += 4
+    else:
+        return False
+
+
+for noun in range(99):
+    for verb in range(99):
+        result = run(memory.copy(), noun, verb, 19690720)
+        if result:
+            print(result)
+            sys.exit(0)
