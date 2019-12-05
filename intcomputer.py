13816a2031dd937bd9ede01d6b1a49494ad3cd5a
Sebastian Höffner <info@sebastian-hoeffner.de>
2019-12-05 12:41:58 +0100

    Updating intcomputer for day 5, pt 2

--- intcomputer.py
+++ intcomputer.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import itertools
+import functools
 from pathlib import Path
 
 
@@ -13,6 +14,10 @@ class IntComputer:
             2: Opcode(2, 4, multiply),
             3: Opcode(3, 2, read_input),
             4: Opcode(4, 2, write_output),
+            5: Opcode(5, 3, jump_if_true),
+            6: Opcode(6, 3, jump_if_false),
+            7: Opcode(7, 4, less_than),
+            8: Opcode(8, 4, equals),
             99: Opcode(99, 1, halt)
         }
 
@@ -37,7 +42,9 @@ class IntComputer:
                     output_buffer.append(output)
             except StopIteration:
                 break
-        return int(''.join(map(str, output_buffer)))
+        if output_buffer:
+            return int(''.join(map(str, output_buffer)))
+        return 0
 
     def __getitem__(self, key):
         return self.memory[key]
@@ -54,26 +61,85 @@ def handle_mode(memory, mode, value):
     raise ValueError(f'Unknown mode {mode}')
 
 
+def _instruction(return_type=None):
+    def funcwrapper(fun):
+        @functools.wraps(fun)
+        def wrapper(memory, modes, *parameters):
+            ret_value = fun(memory, modes, *parameters)
+            if return_type is 'ip':
+                return ret_value, None
+            return None, ret_value
+        return wrapper
+    return funcwrapper
+
+
+def instruction(fun):
+    return _instruction()(fun)
+
+
+def ip_instruction(fun):
+    return _instruction('ip')(fun)
+
+
+def out_instruction(fun):
+    return _instruction('out')(fun)
+
+
+@instruction
 def add(memory, modes, summand0, summand1, result_addr):
     summand0 = handle_mode(memory, modes[0], summand0)
     summand1 = handle_mode(memory, modes[1], summand1)
     memory[result_addr] = summand0 + summand1
 
 
+@instruction
 def multiply(memory, modes, factor0, factor1, result_addr):
     factor0 = handle_mode(memory, modes[0], factor0)
     factor1 = handle_mode(memory, modes[1], factor1)
     memory[result_addr] = factor0 * factor1
 
 
+@instruction
 def read_input(memory, modes, addr):
     memory[addr] = int(sys.stdin.readline().strip())
 
 
+@out_instruction
 def write_output(memory, modes, value):
     return handle_mode(memory, modes[0], value)
 
 
+@ip_instruction
+def jump_if_true(memory, modes, comp, jump_addr):
+    comp = handle_mode(memory, modes[0], comp)
+    jump_addr = handle_mode(memory, modes[1], jump_addr)
+    if comp != 0:
+        return jump_addr
+
+
+@ip_instruction
+def jump_if_false(memory, modes, comp, jump_addr):
+    comp = handle_mode(memory, modes[0], comp)
+    jump_addr = handle_mode(memory, modes[1], jump_addr)
+    if comp == 0:
+        return jump_addr
+
+
+@instruction
+def less_than(memory, modes, comp0, comp1, result_addr):
+    comp0 = handle_mode(memory, modes[0], comp0)
+    comp1 = handle_mode(memory, modes[1], comp1)
+    memory[result_addr] = int(comp0 < comp1)
+
+
+@instruction
+def equals(memory, modes, comp0, comp1, result_addr):
+    comp0 = handle_mode(memory, modes[0], comp0)
+    comp1 = handle_mode(memory, modes[1], comp1)
+    memory[result_addr] = int(comp0 == comp1)
+
+
+@instruction
 def halt(memory, modes):
     raise StopIteration()
 
@@ -91,14 +157,18 @@ class Opcode:
             parameter_modes += [0] * (self.stride - 1 - len(parameter_modes))
 
         parameters = memory[instruction_pointer + 1:instruction_pointer + self.stride]
+        instruction_pointer += self.stride
         try:
             print(f'{self.code} [{self.callfun.__name__}]{parameters}{parameter_modes}')
-            output = self.callfun(memory, parameter_modes, *parameters)
+            ip, output = self.callfun(memory, parameter_modes, *parameters)
+            # print(memory)
+            if ip is not None:
+                instruction_pointer = ip
         except IndexError as e:
             print(f'Error in {self.callfun.__name__}: {parameters}', file=sys.stderr)
             print(memory, instruction_pointer, file=sys.stderr)
             raise e
-        return instruction_pointer + self.stride, output
+        return instruction_pointer, output
 
 
 if __name__ == '__main__':
