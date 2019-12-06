2dc679f40f7c43b3f33bfd05bafeb867792faca8
Sebastian Höffner <info@sebastian-hoeffner.de>
2019-12-06 06:59:55 +0100

    finishing orbit part 2

--- 06/orbits.py
+++ 06/orbits.py
@@ -25,7 +25,37 @@ def count_steps(g, root, node, depth):
         c += count_steps(g, node, n, depth + 1)
     return depth + c
 
+
 c = 0
 for n in g['COM']:
     c += count_steps(g, 'COM', n, 1)
+
 print(c)
+
+
+def find(g, node, search, path=None):
+    if path is None:
+        path = []
+
+    if node == search:
+        return [node]
+
+    if node not in g:
+        return []
+
+    for n in g[node]:
+        p = find(g, n, search)
+        if p:
+            return [node] + p
+    return []
+
+
+path_you = find(g, 'COM', 'YOU')
+path_santa = find(g, 'COM', 'SAN')
+
+root = None
+for n in path_you:
+    if n in path_santa:
+        root = n
+
+print(len(path_you[path_you.index(root)+1:-1]) + len(path_santa[path_santa.index(root)+1:-1]))
