006056d15d88f39a591fb50a5e4afda5c02317db
Sebastian Höffner <info@sebastian-hoeffner.de>
2019-12-09 07:03:01 +0100

    BOOST program.

--- intcomputer.py
+++ intcomputer.py
@@ -9,10 +9,31 @@ LOGFILE = None
 MEMFILE = None
 
 
+class Memory(list):
+    def ensure_length(self, key):
+        if isinstance(key, int):
+            min_size = key
+        else:
+            min_size = key.stop
+        if min_size < 0:
+            raise IndexError('Negative indices are not allowed.')
+        while min_size >= len(self):
+            self.append(0)
+
+    def __setitem__(self, key, value):
+        self.ensure_length(key)
+        super().__setitem__(key, value)
+
+    def __getitem__(self, key):
+        self.ensure_length(key)
+        return super().__getitem__(key)
+
+
 class IntComputer:
     def __init__(self, memory=None):
-        self.memory = memory if memory is not None else []
+        self.memory = Memory(memory if memory is not None else [])
         self.instruction_pointer = 0
+        self.relative_base = 0
         self.opcodes = {
             1: Opcode(1, 4, add),
             2: Opcode(2, 4, multiply),
@@ -22,11 +43,12 @@ class IntComputer:
             6: Opcode(6, 3, jump_if_false),
             7: Opcode(7, 4, less_than),
             8: Opcode(8, 4, equals),
+            9: Opcode(9, 2, relative_base_offset),
             99: Opcode(99, 1, halt)
         }
 
     def __call__(self, verbose=False):
-        max_steps = 10000
+        max_steps = 1000000
         for step in itertools.count():
             if step == max_steps:
                 print(f'Max steps {max_steps} reached', file=LOGFILE)
@@ -40,7 +62,7 @@ class IntComputer:
                 parameter_modes.append(mode)
             op = self.opcodes[opcode]
             try:
-                self.instruction_pointer = op(self.memory, self.instruction_pointer, parameter_modes, verbose)
+                self.instruction_pointer, self.relative_base = op(self.memory, self.instruction_pointer, self.relative_base, parameter_modes, verbose)
             except StopIteration:
                 if PROMPT:
                     print('\n', end=PROMPT, file=sys.stdout)
@@ -54,66 +76,83 @@ class IntComputer:
         self.memory[key] = value
 
 
-def handle_mode(memory, mode, value):
+def handle_mode(memory, mode, relative_base, value):
     if mode == 0:
         return memory[value]
     if mode == 1:
         return value
+    if mode == 2:
+        return memory[value + relative_base]
     raise ValueError(f'Unknown mode {mode}')
 
 
-def add(memory, modes, summand0, summand1, result_addr):
-    summand0 = handle_mode(memory, modes[0], summand0)
-    summand1 = handle_mode(memory, modes[1], summand1)
+def add(memory, modes, relative_base, summand0, summand1, result_addr):
+    if modes[2] == 2:
+        result_addr += relative_base
+    summand0 = handle_mode(memory, modes[0], relative_base, summand0)
+    summand1 = handle_mode(memory, modes[1], relative_base, summand1)
     memory[result_addr] = summand0 + summand1
 
 
-def multiply(memory, modes, factor0, factor1, result_addr):
-    factor0 = handle_mode(memory, modes[0], factor0)
-    factor1 = handle_mode(memory, modes[1], factor1)
+def multiply(memory, modes, relative_base, factor0, factor1, result_addr):
+    if modes[2] == 2:
+        result_addr += relative_base
+    factor0 = handle_mode(memory, modes[0], relative_base, factor0)
+    factor1 = handle_mode(memory, modes[1], relative_base, factor1)
     memory[result_addr] = factor0 * factor1
 
 
-def read_input(memory, modes, addr):
+def read_input(memory, modes, relative_base, addr):
+    if modes[0] == 2:
+        addr += relative_base
     memory[addr] = int(input(PROMPT))
     if LOGFILE:
         print('INPUT:', memory[addr], file=LOGFILE)
 
 
-def write_output(memory, modes, value):
-    value = handle_mode(memory, modes[0], value)
+def write_output(memory, modes, relative_base, value):
+    value = handle_mode(memory, modes[0], relative_base, value)
     print(value, file=sys.stdout, end='')
     if LOGFILE:
         print('OUTPUT:', value, file=LOGFILE)
 
 
-def jump_if_true(memory, modes, comp, jump_addr):
-    comp = handle_mode(memory, modes[0], comp)
-    jump_addr = handle_mode(memory, modes[1], jump_addr)
+def jump_if_true(memory, modes, relative_base, comp, jump_addr):
+    comp = handle_mode(memory, modes[0], relative_base, comp)
+    jump_addr = handle_mode(memory, modes[1], relative_base, jump_addr)
     if comp != 0:
-        return jump_addr
+        return jump_addr, None
 
 
-def jump_if_false(memory, modes, comp, jump_addr):
-    comp = handle_mode(memory, modes[0], comp)
-    jump_addr = handle_mode(memory, modes[1], jump_addr)
+def jump_if_false(memory, modes, relative_base, comp, jump_addr):
+    comp = handle_mode(memory, modes[0], relative_base, comp)
+    jump_addr = handle_mode(memory, modes[1], relative_base, jump_addr)
     if comp == 0:
-        return jump_addr
+        return jump_addr, None
 
 
-def less_than(memory, modes, comp0, comp1, result_addr):
-    comp0 = handle_mode(memory, modes[0], comp0)
-    comp1 = handle_mode(memory, modes[1], comp1)
+def less_than(memory, modes, relative_base, comp0, comp1, result_addr):
+    if modes[2] == 2:
+        result_addr += relative_base
+    comp0 = handle_mode(memory, modes[0], relative_base, comp0)
+    comp1 = handle_mode(memory, modes[1], relative_base, comp1)
     memory[result_addr] = int(comp0 < comp1)
 
 
-def equals(memory, modes, comp0, comp1, result_addr):
-    comp0 = handle_mode(memory, modes[0], comp0)
-    comp1 = handle_mode(memory, modes[1], comp1)
+def equals(memory, modes, relative_base, comp0, comp1, result_addr):
+    if modes[2] == 2:
+        result_addr += relative_base
+    comp0 = handle_mode(memory, modes[0], relative_base, comp0)
+    comp1 = handle_mode(memory, modes[1], relative_base, comp1)
     memory[result_addr] = int(comp0 == comp1)
 
 
-def halt(memory, modes):
+def relative_base_offset(memory, modes, relative_base, adjustment):
+    adjustment = handle_mode(memory, modes[0], relative_base, adjustment)
+    return None, relative_base + adjustment
+
+
+def halt(memory, modes, relative_base):
     raise StopIteration()
 
 
@@ -123,7 +162,7 @@ class Opcode:
         self.stride = stride
         self.callfun = callfun
 
-    def __call__(self, memory, instruction_pointer, parameter_modes=None, verbose=False):
+    def __call__(self, memory, instruction_pointer, relative_base=0, parameter_modes=None, verbose=False):
         if parameter_modes is None:
             parameter_modes = [0] * (self.stride - 1)
         elif len(parameter_modes) < (self.stride - 1):
@@ -134,16 +173,19 @@ class Opcode:
         try:
             if verbose:
                 print(f'{self.code} [{self.callfun.__name__}]{parameters}{parameter_modes}', file=LOGFILE)
-            ip = self.callfun(memory, parameter_modes, *parameters)
+            ip_and_base = self.callfun(memory, parameter_modes, relative_base, *parameters)
             if MEMFILE:
                 print(memory, file=MEMFILE)
-            if ip is not None:
-                instruction_pointer = ip
+            if ip_and_base is not None:
+                if ip_and_base[0] is not None:
+                    instruction_pointer = ip_and_base[0]
+                if ip_and_base[1] is not None:
+                    relative_base = ip_and_base[1]
         except IndexError as e:
             print(f'Error in {self.callfun.__name__}: {parameters}', file=LOGFILE)
             print(memory, instruction_pointer, file=LOGFILE)
             raise e
-        return instruction_pointer
+        return instruction_pointer, relative_base
 
 
 def main(sys_argv=sys.argv):
