1ff348b52481e59d15866a85b59d5cc4963cb3bf
Sebastian Höffner <info@sebastian-hoeffner.de>
2019-12-12 04:17:15 +0100

    Rewriting IntComputer to not rely on heavy I/O anymore.

--- intcomputer.py
+++ intcomputer.py
@@ -1,12 +1,39 @@
-import sys
-import argparse
-import itertools
+import time
+import threading
+import queue
+from functools import lru_cache
+from enum import IntEnum
 from pathlib import Path
 
 
-PROMPT = ''
-LOGFILE = None
-MEMFILE = None
+def parse_program(string):
+    return map(int, string.split(','))
+
+
+def load_program(program_file_name):
+    return parse_program((Path(__file__).parent / program_file_name).read_text())
+
+
+class ParameterTypes(IntEnum):
+    READ = 0
+    WRITE = 1
+
+
+class Opcode:
+    def __init__(self, function, *parameter_types):
+        self.function = function
+        self.parameter_types = parameter_types
+
+    @property
+    @lru_cache()
+    def stride(self):
+        return len(self.parameter_types) + 1
+
+    def __call__(self, *params):
+        return self.function(*params)
+
+    def __str__(self):
+        return f'[{self.function.__name__.split("_")[-1]}/{self.stride - 1}]'
 
 
 class Memory(list):
@@ -29,186 +56,131 @@ class Memory(list):
         return super().__getitem__(key)
 
 
-class IntComputer:
+class IntComputer(threading.Thread):
     def __init__(self, memory=None):
-        self.memory = Memory(memory if memory is not None else [])
         self.instruction_pointer = 0
         self.relative_base = 0
+        self.memory = Memory(memory if memory is not None else [99])
+        self._terminated = False
+        self.stdin = queue.Queue(1)
+        self.stdout = queue.Queue(1)
+        self.lock = threading.Semaphore()
+        R = ParameterTypes.READ
+        W = ParameterTypes.WRITE
         self.opcodes = {
-            1: Opcode(1, 4, add),
-            2: Opcode(2, 4, multiply),
-            3: Opcode(3, 2, read_input),
-            4: Opcode(4, 2, write_output),
-            5: Opcode(5, 3, jump_if_true),
-            6: Opcode(6, 3, jump_if_false),
-            7: Opcode(7, 4, less_than),
-            8: Opcode(8, 4, equals),
-            9: Opcode(9, 2, relative_base_offset),
-            99: Opcode(99, 1, halt)
+            1: Opcode(self._op_add, R, R, W),
+            2: Opcode(self._op_multiply, R, R, W),
+            3: Opcode(self._op_input, W),
+            4: Opcode(self._op_output, R),
+            5: Opcode(self._op_jump_if_true, R, R),
+            6: Opcode(self._op_jump_if_false, R, R),
+            7: Opcode(self._op_less_than, R, R, W),
+            8: Opcode(self._op_equals, R, R, W),
+            9: Opcode(self._op_relative_base_offset, R),
+            99: Opcode(self._op_halt)
         }
 
-    def __call__(self, verbose=False):
-        max_steps = 1000000
-        for step in itertools.count():
-            if step == max_steps:
-                print(f'Max steps {max_steps} reached', file=LOGFILE)
-                break
-            instruction = self.memory[self.instruction_pointer]
-            instruction, opcode = divmod(instruction, 100)
+    def _parse_instruction(self, instruction):
+        instruction, opcode = divmod(instruction, 100)
+        op = self.opcodes[opcode]
+        parameters = []
+        for i, t in zip(range(1, op.stride), op.parameter_types):
+            instruction, mode = divmod(instruction, 10)
+            if mode == 0:
+                addr = self.memory[self.instruction_pointer + i]
+                if t == ParameterTypes.READ:
+                    parameters.append(self.memory[addr])
+                else:
+                    parameters.append(addr)
+            elif mode == 1:
+                value = self.memory[self.instruction_pointer + i]
+                parameters.append(value)
+            elif mode == 2:
+                addr = self.memory[self.instruction_pointer + i] + self.relative_base
+                if t == ParameterTypes.READ:
+                    parameters.append(self.memory[addr])
+                else:
+                    parameters.append(addr)
+        return op, parameters
 
-            parameter_modes = []
-            while instruction:
-                instruction, mode = divmod(instruction, 10)
-                parameter_modes.append(mode)
-            op = self.opcodes[opcode]
+    @property
+    def terminated(self):
+        self.lock.acquire()
+        t = self._terminated
+        self.lock.release()
+        return t
+
+    @terminated.setter
+    def terminated(self, value):
+        self.lock.acquire()
+        self._terminated = value
+        self.lock.release()
+
+    def __call__(self):
+        while not self.terminated:
             try:
-                self.instruction_pointer, self.relative_base = op(self.memory, self.instruction_pointer, self.relative_base, parameter_modes, verbose)
+                instruction = self.memory[self.instruction_pointer]
+                op, parameters = self._parse_instruction(instruction)
+                self.instruction_pointer += op.stride
+                op(*parameters)
             except StopIteration:
-                if PROMPT:
-                    print('\n', file=sys.stdout)
-                break
-        return 0
+                self.terminated = True
 
-    def __getitem__(self, key):
-        return self.memory[key]
+    def thread(self):
+        return threading.Thread(target=self.__call__, name='IntComputerProcess')
 
-    def __setitem__(self, key, value):
-        self.memory[key] = value
+    def __enter__(self):
+        self._thread = self.thread()
+        self._thread.start()
+        return self
 
+    def __exit__(self, error, value, traceback):
+        self._thread.join()
 
-def handle_mode(memory, mode, relative_base, value):
-    if mode == 0:
-        return memory[value]
-    if mode == 1:
-        return value
-    if mode == 2:
-        return memory[value + relative_base]
-    raise ValueError(f'Unknown mode {mode}')
+    def _op_add(self, a, b, c):
+        self.memory[c] = a + b
 
+    def _op_multiply(self, a, b, c):
+        self.memory[c] = a * b
 
-def add(memory, modes, relative_base, summand0, summand1, result_addr):
-    if modes[2] == 2:
-        result_addr += relative_base
-    summand0 = handle_mode(memory, modes[0], relative_base, summand0)
-    summand1 = handle_mode(memory, modes[1], relative_base, summand1)
-    memory[result_addr] = summand0 + summand1
+    def _op_input(self, a):
+        self.memory[a] = self.stdin.get()
 
+    def _op_output(self, a):
+        self.stdout.put(a)
 
-def multiply(memory, modes, relative_base, factor0, factor1, result_addr):
-    if modes[2] == 2:
-        result_addr += relative_base
-    factor0 = handle_mode(memory, modes[0], relative_base, factor0)
-    factor1 = handle_mode(memory, modes[1], relative_base, factor1)
-    memory[result_addr] = factor0 * factor1
+    def _op_jump_if_true(self, a, b):
+        if a != 0:
+            self.instruction_pointer = b
 
+    def _op_jump_if_false(self, a, b):
+        if a == 0:
+            self.instruction_pointer = b
 
-def read_input(memory, modes, relative_base, addr):
-    if modes[0] == 2:
-        addr += relative_base
-    memory[addr] = int(input(PROMPT))
-    if LOGFILE:
-        print('INPUT:', memory[addr], file=LOGFILE)
+    def _op_less_than(self, a, b, c):
+        self.memory[c] = int(a < b)
 
+    def _op_equals(self, a, b, c):
+        self.memory[c] = int(a == b)
 
-def write_output(memory, modes, relative_base, value):
-    value = handle_mode(memory, modes[0], relative_base, value)
-    print(value, file=sys.stdout, end='')
-    if LOGFILE:
-        print('OUTPUT:', value, file=LOGFILE)
+    def _op_relative_base_offset(self, a):
+        self.relative_base += a
+
+    def _op_halt(self):
+        raise StopIteration()
 
 
-def jump_if_true(memory, modes, relative_base, comp, jump_addr):
-    comp = handle_mode(memory, modes[0], relative_base, comp)
-    jump_addr = handle_mode(memory, modes[1], relative_base, jump_addr)
-    if comp != 0:
-        return jump_addr, None
-
-
-def jump_if_false(memory, modes, relative_base, comp, jump_addr):
-    comp = handle_mode(memory, modes[0], relative_base, comp)
-    jump_addr = handle_mode(memory, modes[1], relative_base, jump_addr)
-    if comp == 0:
-        return jump_addr, None
-
-
-def less_than(memory, modes, relative_base, comp0, comp1, result_addr):
-    if modes[2] == 2:
-        result_addr += relative_base
-    comp0 = handle_mode(memory, modes[0], relative_base, comp0)
-    comp1 = handle_mode(memory, modes[1], relative_base, comp1)
-    memory[result_addr] = int(comp0 < comp1)
-
-
-def equals(memory, modes, relative_base, comp0, comp1, result_addr):
-    if modes[2] == 2:
-        result_addr += relative_base
-    comp0 = handle_mode(memory, modes[0], relative_base, comp0)
-    comp1 = handle_mode(memory, modes[1], relative_base, comp1)
-    memory[result_addr] = int(comp0 == comp1)
-
-
-def relative_base_offset(memory, modes, relative_base, adjustment):
-    adjustment = handle_mode(memory, modes[0], relative_base, adjustment)
-    return None, relative_base + adjustment
-
-
-def halt(memory, modes, relative_base):
-    raise StopIteration()
-
-
-class Opcode:
-    def __init__(self, code, stride, callfun):
-        self.code = code
-        self.stride = stride
-        self.callfun = callfun
-
-    def __call__(self, memory, instruction_pointer, relative_base=0, parameter_modes=None, verbose=False):
-        if parameter_modes is None:
-            parameter_modes = [0] * (self.stride - 1)
-        elif len(parameter_modes) < (self.stride - 1):
-            parameter_modes += [0] * (self.stride - 1 - len(parameter_modes))
-
-        parameters = memory[instruction_pointer + 1:instruction_pointer + self.stride]
-        instruction_pointer += self.stride
-        try:
-            if verbose:
-                print(f'{self.code} [{self.callfun.__name__}]{parameters}{parameter_modes}', file=LOGFILE)
-            ip_and_base = self.callfun(memory, parameter_modes, relative_base, *parameters)
-            if MEMFILE:
-                print(memory, file=MEMFILE)
-            if ip_and_base is not None:
-                if ip_and_base[0] is not None:
-                    instruction_pointer = ip_and_base[0]
-                if ip_and_base[1] is not None:
-                    relative_base = ip_and_base[1]
-        except IndexError as e:
-            print(f'Error in {self.callfun.__name__}: {parameters}', file=LOGFILE)
-            print(memory, instruction_pointer, file=LOGFILE)
-            raise e
-        return instruction_pointer, relative_base
-
-
-def main(sys_argv=sys.argv):
-    global PROMPT, LOGFILE, MEMFILE
-    parser = argparse.ArgumentParser()
-    parser.add_argument('program', type=lambda x: list(map(int, Path(x).read_text().split(','))))
-    parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('-p', '--prompt', nargs='?', default=PROMPT)
-    parser.add_argument('-l', '--logfile', nargs='?', type=argparse.FileType('w'))
-    parser.add_argument('-m', '--memfile', nargs='?', type=argparse.FileType('w'))
-    args = parser.parse_args()
-    PROMPT = args.prompt
-    LOGFILE = args.logfile
-    MEMFILE = args.memfile
-
-    if args.verbose:
-        print(f'Program: {args.program}', file=LOGFILE)
-    computer = IntComputer(args.program)
-    computer(verbose=args.verbose)
-    if args.verbose:
-        print(f'Memory: {computer.memory}', file=LOGFILE)
-    return 0
+def main():
+    ic = IntComputer(load_program('day5.input'))
+    with ic as proc:
+        proc.stdin.put(1)
+        while not ic.terminated:
+            time.sleep(0.1)
+            if not proc.stdout.empty():
+                out = proc.stdout.get()
+                print(out)
+    print(ic.memory)
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    main()
