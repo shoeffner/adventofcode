5e9ff6e1ab2a767a6bc5a861b16f001f9ded75c2
Sebastian Höffner <info@sebastian-hoeffner.de>
2021-12-15 01:58:05 +0100

    11.2

---
+++ 2021/11/sync.jl
@@ -0,0 +1,41 @@
+function octoflash()
+    lines = readlines()
+    octos = transpose(reshape(parse.(Int, convert.(String, vcat(split.(lines, "")...))), (:, length(lines))))
+
+    for step in Iterators.countfrom(1)
+        flashed = zeros(Bool, size(octos))
+        octos .+= 1
+
+        tmpflash = -1
+
+        maxc = size(octos, 2)
+        maxr = size(octos, 1)
+        while tmpflash != sum(flashed)
+            newoctos = copy(octos)
+            tmpflash = sum(flashed)
+            for c in 1:maxc
+                for r in 1:maxr
+                    if octos[c, r] > 9 && !flashed[c, r]
+                        flashed[c, r] = true
+                        newoctos[max(c-1, 1):min(c+1, maxc), max(r-1, 1):min(r+1, maxr)] .+= 1
+                    end
+                end
+            end
+            octos = newoctos
+        end
+        octos[octos .> 9] .= 0
+
+        if sum(octos) == 0
+            return step
+        end
+
+    end
+end
+
+
+function main()
+    println(octoflash())
+end
+
+
+main()
