2f5fd68973f8a93fffa3babd17d8081e95232b93
Sebastian Höffner <info@sebastian-hoeffner.de>
2021-12-16 03:05:38 +0100

    13.1

---
+++ 2021/13/origami.jl
@@ -0,0 +1,67 @@
+function readinput()
+    coords = []
+    folds = []
+
+    mode = 0
+
+    for line in readlines()
+        if line == ""
+            mode = 1
+            continue
+        end
+        if mode == 0
+            push!(coords, parse.(Int, convert.(String, split(line, ","))))
+        elseif mode == 1
+            push!(folds, convert.(String, split(split(line, " ")[3], "=")))
+        end
+    end
+
+    coords = transpose(reshape(hcat(coords...), 2, :)) .+ 1
+    return coords, folds
+end
+
+function makegrid(coords)
+    x = maximum(coords[:, 1])
+    y = maximum(coords[:, 2])
+    grid = zeros(Bool, y, x)
+    for (x, y) in eachrow(coords)
+        grid[y, x] = true
+    end
+    return grid
+end
+
+function foldgrid(grid, direction, index)
+    # println(direction, " ", index)
+    if direction == "y"
+        bottom = grid[begin:index - 1, :]
+        top = grid[end:-1:index + 1, :]
+    else
+        bottom = grid[:, begin:index - 1]
+        top = grid[:, end:-1:index + 1]
+    end
+    # println("GRID")
+    # display(grid)
+    # println()
+    # println("BOTTOM")
+    # display(bottom)
+    # println()
+    # println("TOP")
+    # display(top)
+    # println()
+    # println()
+    # println()
+    bottom .| top
+end
+
+function main()
+    coords, folds = readinput()
+    grid = makegrid(coords)
+
+    for f in folds
+         grid = foldgrid(grid, f[1], parse(Int, f[2]) + 1)
+         break
+    end
+    println(sum(grid))
+end
+
+main()
