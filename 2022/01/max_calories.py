d3a6a8c4d79e41a1db95d00e2aca0ad368d94a9e
Sebastian Höffner <info@sebastian-hoeffner.de>
2024-03-29 14:22:49 +0100

    codegolfing

--- 2022/01/max_calories.py
+++ 2022/01/max_calories.py
@@ -1,18 +1,7 @@
 from pathlib import Path
 
-elves = []
-calories = 0
-for line in Path('input').read_text().splitlines():
-    if line:
-        calories += int(line)
-    else:
-        elves.append(calories)
-        calories = 0
+elves = [sum(map(int, elf.splitlines())) for elf in Path('input').read_text().split('\n\n')]
 
 print(max(elves))
-total = 0
-for i in range(3):
-    highest = max(elves)
-    elves.remove(highest)
-    total += highest
-print(total)
+
+print(sum(sorted(elves)[-3:]))
